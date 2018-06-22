bb72136da23d6da84b63f3c83e7440e618c0a559
Ryan Manro <rmanro@gmail.com>
2018-06-22 13:40:35 -0700

    scaffolding

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const app = require('./lib/app');
+const connect = require('./lib/connect');
+
+connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/boommovies');
+
+const server = http.createServer(app);
+const port = process.env.port || 3000;
+
+server.listen(port, () => {
+    // eslint-disable-next-line
+  console.log('Server Running On', server.address().port);
+});
