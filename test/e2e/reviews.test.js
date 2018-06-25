6e6167500f9b52678b9ae9e25fff8ed3373b2ac2
Ryan Manro <rmanro@gmail.com>
2018-06-25 15:10:16 -0700

    Get reviews by User testing OK

--- test/e2e/reviews.test.js
+++ test/e2e/reviews.test.js
@@ -5,6 +5,7 @@ const { dropCollection } = require('./db');
 describe('Reviews e2e', () => {
 
     let _id = null;
+    let reviewId = null;
 
     const review1 = {
         movieId: '555534465463',
@@ -45,8 +46,26 @@ describe('Reviews e2e', () => {
                 return request
                     .get(`/api/reviews/movie/${review1.movieId}`)
                     .then(({ body }) => {
+                        reviewId = body[0]._id;
                         assert.equal(body[0].movieId, review1.movieId);
                     });
+            });
+    });
+
+    it('Posting a Review that already exists fails', () => {
+        return request
+            .post(`/api/reviews/user/${_id}`)
+            .send(review1)
+            .then(res => {
+                assert.equal(res.error.status, 400);
+            });
+    });
+
+    it('Gets Reviews by User', () => {
+        return request
+            .get(`/api/reviews/user/${_id}`)
+            .then(({ body }) => {
+                assert.deepEqual(body[0], { ...review1, __v: 0, _id: reviewId });
             });
     });
 });
